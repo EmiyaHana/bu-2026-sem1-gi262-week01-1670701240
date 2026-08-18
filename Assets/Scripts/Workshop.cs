853e8731b317365dbb5f2c49048b91675a901610
EmiyaHana <warm310149@gmail.com>
2026-08-18 10:45:26 +0700

    VerifyIdentify_Comply

--- Assets/Scripts/Workshop.cs
+++ Assets/Scripts/Workshop.cs
@@ -11,8 +11,8 @@ public class Workshop : MonoBehaviour
         // As03_NumberComparisonExample();
         // As04_AndOrOperatorExample();
         // As05_GuessingNumberExample();
-        As06_GuessingNumberMoreOrLessExample();
-        // As07_VerifyIdentityExample();
+        // As06_GuessingNumberMoreOrLessExample();
+        As07_VerifyIdentityExample();
     }
 
     public bool isSixOClock;
@@ -118,6 +118,26 @@ public class Workshop : MonoBehaviour
     public bool as07IsPaid;
     public void As07_VerifyIdentityExample()
     {
+        if (as07Username == "User" && as07Password == "user123")
+        {
+            Debug.Log("You have accessed.");
 
+            if (as07IsPaid)
+            {
+                Debug.Log("VIP member.");
+            }
+            else
+            {
+                Debug.Log("Free member.");
+            }
+            if (as07Age > 18)
+            {
+                Debug.Log("You have access to exclusive content.");
+            }
+        }
+        else
+        {
+            Debug.Log("You are guest.");
+        }
     }
 }
